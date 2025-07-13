5022c828bb8d67e8fb8554c4ca665162aacfb3ad
sweeetpotaooo <sehyun5004@naver.com>
2025-07-14 00:13:13 +0900

    [feat]대시보드 화면 제구성

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -1,4 +1,7 @@
 describe('홈 화면', () => {
+  beforeEach(() => {
+    cy.login('test@user.com', 'password123');
+  });
   it('메인 페이지가 정상적으로 렌더링된다', () => {
     cy.visit('/');
     cy.contains('홈').should('exist');
--- cypress/e2e/learn.cy.ts
+++ cypress/e2e/learn.cy.ts
@@ -1,4 +1,7 @@
 describe('학습 페이지', () => {
+  beforeEach(() => {
+    cy.login('test@user.com', 'password123');
+  });
   it('학습 화면이 정상적으로 렌더링된다', () => {
     cy.visit('/learn');
     cy.contains('학습').should('exist');
